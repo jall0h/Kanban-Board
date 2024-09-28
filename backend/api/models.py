ae1bb34c403a9a350bb437f3886cb75965760a9c
Abu-Bakarr Jalloh <k22010427@kcl.ac.uk>
2024-09-28 16:00:41 +0100

    Add User Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
+class User(AbstractUser):
+    """Model used for user authentication, and team member related information."""
+
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        validators=[RegexValidator(
+            regex=r'^@\w{3,}$',
+            message='Username must consist of @ followed by at least three alphanumericals'
+        )]
+    )
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+
+    class Meta:
+        """Model options."""
+        ordering = ['last_name', 'first_name']
+
+    def full_name(self):
+        """Return a string containing the user's full name."""
+        return f'{self.first_name} {self.last_name}'
+    
