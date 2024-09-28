ae402860676229ad3f2fcb8a92bc4a514a9557df
Abu-Bakarr Jalloh <k22010427@kcl.ac.uk>
2024-09-28 16:15:23 +0100

    Create Task model and migrate models

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 
-# Create your models here.
 class User(AbstractUser):
     """Model used for user authentication, and team member related information."""
 
@@ -26,3 +25,13 @@ class User(AbstractUser):
         """Return a string containing the user's full name."""
         return f'{self.first_name} {self.last_name}'
     
+class Task(models.Model):
+    TASK_STATUS_CHOICES = [
+        ("TODO","Todo"),
+        ("PROG","In Progress"),
+        ("DONE", "Done")
+        ]
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField(max_length=250, blank=True)
+    status = models.CharField(max_length=5, choices=TASK_STATUS_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
